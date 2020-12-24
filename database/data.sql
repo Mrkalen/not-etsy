0aaaeb3af86e9c6dfd411fa0c4194ecc3dd2befb
MrKalen <mrkalen@hotmail.com>
2020-12-23 16:00:46 -0800

    Added all database data needed for the project. Created new database, added data to database. Started working on react page.

---
+++ database/data.sql
@@ -0,0 +1,38 @@
+insert into "categories" ("label")
+values ('ornaments'),
+       ('drinkware'),
+       ('wall-decor'),
+       ('misc');
+
+insert into "customization" ("label")
+values ('name'),
+       ('name and date'),
+       ('brand'),
+       ('custom');
+
+
+insert into "products" ("productName", "description", "price", "pictureUrl", "categoryId", "customizationId")
+values (
+        'Snoopy and his best friend',
+        'Four custom ornaments with snoopy and his best friend Charlie Brown',
+        5.00,
+        '/images/snoopy.jpg',
+        1,
+        4
+        ),
+       (
+        'In this house',
+        'Made from the finest pallets the McDonalds down the stree was throwing away. Aged by yours truly with old paint I found in my garage.',
+        5.00,
+        '/images/custom-this-house-team.jpg',
+        3,
+        3
+        ),
+       (
+        'Family',
+        'Made from the thickest pieces of cardboard I could find, this print will melt your heart!',
+        5.00,
+        '/images/framed-family.jpg',
+        3,
+        2
+        );
--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(staticMiddleware);
 
+app.get('/api/products', (req, res, next) => {
+  // console.log(req.body);
+});
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
