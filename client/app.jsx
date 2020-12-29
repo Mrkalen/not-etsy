66a264f17120f271ac4eb0f5b41c92374c0b7082
MrKalen <mrkalen@hotmail.com>
2020-12-28 18:25:38 -0800

    Added new component, added route for single product, updated parse route to get productId

--- client/app.jsx
+++ client/app.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Header from './header';
 import NewItems from './pages/new-items';
+import parseRoute from '../server/parse-route';
+import Custom1 from './pages/custom1';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      route: parseRoute(window.location.hash),
+      currentProduct: {}
     };
 
   }
 
   componentDidMount() {
     this.getProducts();
+    window.addEventListener('hashchange', () => {
+      const newRoute = parseRoute(window.location.hash);
+      this.setState({ route: newRoute });
+    });
+    this.getProduct();
   }
 
   getProducts() {
@@ -22,19 +31,45 @@ export default class App extends React.Component {
       .catch(err => console.error('GET error', err.message));
   }
 
-  render() {
+  getProduct() {
+    const prodId = Number(this.state.route.id);
+    fetch(`/api/product/${prodId}`)
+      .then(res => res.json())
+      .then(product => {
+        this.setState({ currentProduct: product });
+      })
+      .catch(err => console.error('GET error', err.message));
+  }
+
+  renderPage() {
+
+    const { route } = this.state;
     const products = this.state.products;
+
+    if (route.path === '') {
+      return (
+      <div className='row'>
+      {
+    products.reverse().map((product, index) => {
+      return <NewItems key={index} product={product} />;
+    })
+  }
+  </div>
+      );
+    } else if (route.path === 'details4') {
+      return (
+        <Custom1 product={this.state.currentProduct} />
+      );
+    }
+  }
+
+  render() {
+
     return (
       <>
       <Header />
-        <div className='row'>
-      {
-      products.reverse().map((product, index) => {
-        return <NewItems key={index} item={product.productName} price={product.price} img={product.pictureUrl}/>;
-      })
+      { this.renderPage() }
 
-      }
-      </div>
       </>
     );
   }
--- client/pages/custom1.jsx
+++ client/pages/custom1.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
-export default function NewItems(props) {
+export default function Custom1(props) {
+
   const image = props.product.pictureUrl;
   const title = props.product.productName;
   const price = props.product.price;
-  const customization = props.product.customizationId;
-  const productId = props.product.productId;
   return (
     <div className='col-6'>
-      <a href={`#details${customization}?productId=${productId}`}>
         <div className="card mb-3 shadow-sm text-center">
           <img src={image}></img>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-          <h5 className='price'>{`$${price}`}</h5>
+            <h5 className='price'>{`$${price}`}</h5>
           </div>
         </div>
-        </a>
-      </div>
+    </div>
   );
 }
