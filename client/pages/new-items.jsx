4b3a686c84261d9846316854d6c4e1db05319848
MrKalen <mrkalen@hotmail.com>
2020-12-29 15:50:42 -0800

    Moved the fetch for the list of products to the new-items module and adjusted the code to make it work in there.

--- client/pages/new-items.jsx
+++ client/pages/new-items.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
-function Product(props) {
-  const { pictureUrl, productName, price, productId } = props.product;
-  return (
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(products => this.setState({ products }))
+      .catch(err => console.error('GET error', err.message));
+  }
+
+  render() {
+    const { pictureUrl, productName, price, productId } = this.props.product;
+    return (
     <div className='col-6'>
       <a href={`#details?productId=${productId}`}>
         <div className="card mb-3 shadow-sm text-center">
           <img className='img-no-detail' src={pictureUrl}></img>
           <div className="card-body">
             <h5 className="card-title">{productName}</h5>
-            <h5 className='price'>{`$${price}`}</h5>
+            <h5 className='price'>{`$${price}.00`}</h5>
           </div>
         </div>
       </a>
     </div>
-  );
+    );
+  }
 }
 
 function NewItems(props) {
