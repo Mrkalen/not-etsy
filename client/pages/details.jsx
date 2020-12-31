c6cce646eb9f0329eb81024e72e086f49825856e
MrKalen <mrkalen@hotmail.com>
2020-12-31 08:57:16 -0800

    added button on all forms for user to submit customizations. Added quantity button for users to add quantity of items.

--- client/pages/details.jsx
+++ client/pages/details.jsx
@@ -26,52 +26,97 @@ export default class Details extends React.Component {
     const { customizationId } = this.state.currentProduct;
     if (customizationId === 4) {
       return (
-        <div className='text-left m-3'>
-          <label htmlFor='custom-request' className='custom-request'>
-            Please enter any custom details:
+        <form>
+          <div className='text-left m-3'>
+            <label htmlFor='custom-request' className='custom-request'>
+              Please enter any custom details:
             </label>
-          <textarea className='shadow' cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
-        </div>
+            <textarea className='shadow' cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
+          </div>
+          <div className='row d-flex justify-content-around align-items-center'>
+            <div className="input-group col-4">
+              <select className="custom-select shadow pr-3 qty" id="inputGroupSelect03" aria-label="Example select with button addon">
+                <option default>QTY</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary col-4 p-0 shadow">Add to cart</button>
+          </div>
+        </form>
       );
     } else if (customizationId === 3) {
       return (
-        <div className='text-left m-3'>
-          <div className="input-group mb-2 w-50 mx-auto">
-            <select required className="custom-select team-name shadow" id="inputGroupSelect01">
-              <option defaultValue>Team Name</option>
-              <option value="1">Ram&apos;s</option>
-              <option value="2">Chargers</option>
-              <option value="3">Raiders</option>
-              <option value="4">49ers</option>
-              <option value="5">Seahawks</option>
-              <option value="6">Dolphins</option>
-            </select>
-          </div>
-          <label htmlFor='custom-request' className='custom-request'>
-            Please enter any custom details:
+        <form>
+          <div className='text-left m-3'>
+            <div className="input-group mb-2 w-50 mx-auto">
+              <select required className="custom-select team-name shadow" id="inputGroupSelect01">
+                <option defaultValue>Team Name</option>
+                <option value="1">Ram&apos;s</option>
+                <option value="2">Chargers</option>
+                <option value="3">Raiders</option>
+                <option value="4">49ers</option>
+                <option value="5">Seahawks</option>
+                <option value="6">Dolphins</option>
+              </select>
+            </div>
+            <label htmlFor='custom-request' className='custom-request'>
+              Please enter any custom details:
             </label>
-          <textarea className="shadow" cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
-        </div>
+            <textarea className="shadow" cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
+            <div className='row d-flex justify-content-around align-items-center'>
+              <div className="input-group col-4">
+                <select className="custom-select shadow pr-3 qty" id="inputGroupSelect03" aria-label="Example select with button addon">
+                  <option default>QTY</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </div>
+            <button type="submit" className="btn btn-primary col-4 p-0 shadow">Add to cart</button>
+            </div>
+          </div>
+        </form>
       );
     } else if (customizationId === 2) {
       return (
-        <div className='text-left m-3'>
-          <label htmlFor='custom-name-date' className='custom-name-date'>
-            Please enter your last name and a special date:
+        <form>
+          <div className='text-left m-3'>
+            <label htmlFor='custom-name-date' className='custom-name-date'>
+              Please enter your last name and an est date:
           </label>
-          <div className='custom-name-date-input d-flex justify-content-between'>
-            <div className="input-group input-group-sm mb-3 pl-0 col-6">
-              <input type="text" className="form-control shadow" placeholder='Name' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+            <div className='custom-name-date-input d-flex justify-content-between'>
+              <div className="input-group input-group-sm mb-3 pl-0 col-6">
+                <input type="text" className="form-control shadow" placeholder='Name' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+              </div>
+              <div className="input-group input-group-sm mb-3 pr-0 col-6 ">
+                <input type="text" className="form-control shadow" placeholder='Date' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+              </div>
             </div>
-            <div className="input-group input-group-sm mb-3 pr-0 col-6 ">
-              <input type="text" className="form-control shadow" placeholder='Date' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
-            </div>
-          </div>
-          <label htmlFor='custom-request' className='custom-request'>
-            Please enter any custom details:
+            <label htmlFor='custom-request' className='custom-request'>
+              Please enter any custom details:
             </label>
-          <textarea className='shadow' cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
-        </div>
+            <textarea className='shadow' cols='37' rows='3' type='text' id='custom-reqest' name='custom-request' placeholder='Color, name, phrase, ...'></textarea>
+          </div>
+          <div className='row d-flex justify-content-around align-items-center'>
+            <div className="input-group col-4">
+              <select className="custom-select shadow pr-3 qty" id="inputGroupSelect03" aria-label="Example select with button addon">
+                <option default>QTY</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+          <button type="submit" className="btn btn-primary col-4 p-0 shadow">Add to cart</button>
+          </div>
+        </form>
       );
     }
   }
