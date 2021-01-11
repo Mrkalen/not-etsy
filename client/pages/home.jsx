e331d71ed37ce5fabdf5c60cc3db451497b5c1eb
MrKalen <mrkalen@hotmail.com>
2021-01-10 16:20:12 -0800

    started working on home page. Created new component that renders the thumbnails of each picture for each

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 
-export default function Home(props) {
+function NewThumbnails(props) {
+  console.log(props.products);
   return (
-    <>
-    </>
+    props.products.map((product, index) => {
+      return (
+        <>
+          <div className='col-4' key={`product-${props.productId}`}>
+            <a>
+              <img src={product.pictureUrl} className='img-thumbnail img-fluid'></img>
+            </a>
+          </div>
+        </>
+      );
+    }
+
+    )
   );
+}
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(products => this.setState({ products }))
+      .catch(err => console.error('GET error', err.message));
+  }
+
+  render() {
+    return (
+      <>
+        <div className='carousel-title text-center'>
+          <h2>New Items</h2>
+        </div>
+        <div className='new-items row d-flex align-items-center'>
+          <NewThumbnails products={this.state.products} />
+        </div>
+      </>
+    );
+  }
 }
