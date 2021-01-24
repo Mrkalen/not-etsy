9faead67d765700b4be0dfced9937ace2905db5c
MrKalen <mrkalen@hotmail.com>
2021-01-24 13:32:47 -0800

    Moved all logic into thumbnailGallery. Changed thumbnailGallery from function to a class. Added handlers and stateto here

--- client/thumbnail-gallery.jsx
+++ client/thumbnail-gallery.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
-export default function ThumbnailGallery(props) {
-  const { chevronClick, titleLink, title, name } = props;
-  return (
+export default class ThumbnailGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carouselIndex: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const id = event.target.id;
+    let newState = Object.create(this.state.carouselIndex, {});
+    if (id === 'chevRight') {
+      this.setState({ carouselIndex: newState += 1 });
+    } else if (id === 'chevLeft') {
+      this.setState({ carouselIndex: newState -= 1 });
+    }
+  }
+
+  render() {
+    const { titleLink, title, products } = this.props;
+    return (
     <>
       <div className='carousel-title text-center'>
         <a href={titleLink}>
@@ -10,25 +29,14 @@ export default function ThumbnailGallery(props) {
         </a>
       </div>
       <div className='d-flex flex-row align-items-center justify-content-between'>
-      <i onClick={chevronClick} className="fas fa-chevron-left pr-2" id={`${name}Left`}></i>
+      <i onClick={this.handleClick} className="fas fa-chevron-left pr-2" id='chevLeft'></i>
       <div className='row d-flex justify-content-around'>
-        {props.products.map((product, index) => {
-          const arrLength = props.products.length;
-          const carouselIndex = props.carouselIndex;
-          let display = 'd-none';
-          console.log('arrLength', arrLength - 3);
-          console.log('index', index);
-          console.log('carouselIndex', carouselIndex);
-          if (index >= 0 && carouselIndex <= arrLength - 3) {
+        {products.map((product, index) => {
+          console.log(product);
+          console.log(product.productId);
+          const arrLength = product.length;
+          const display = '';
 
-            if (index === carouselIndex || index === carouselIndex + 1 || index === carouselIndex + 2) {
-              display = '';
-            }
-          } else if (index === arrLength - 2) {
-            if (index === carouselIndex || index === carouselIndex + 1) {
-              display = '';
-            }
-          }
           return (
             <div className={display} key={product.productId}>
               <div className='col-4 justify-content-center p-0'>
@@ -40,8 +48,9 @@ export default function ThumbnailGallery(props) {
           );
         })}
       </div>
-        <i onClick={chevronClick} className='fas fa-chevron-right pl-2' id={`${name}Right`}></i>
+        <i onClick={this.handleClick} className='fas fa-chevron-right pl-2' id='chevRight'></i>
       </div>
     </>
-  );
+    );
+  }
 }
