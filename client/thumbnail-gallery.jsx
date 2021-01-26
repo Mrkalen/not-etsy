41efdae4672f26dc96ce77915ec21d2ea197ceab
MrKalen <mrkalen@hotmail.com>
2021-01-26 10:02:25 -0800

    Completed carousel

--- client/thumbnail-gallery.jsx
+++ client/thumbnail-gallery.jsx
@@ -4,9 +4,7 @@ export default class ThumbnailGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstPosition: 0,
-      secondPosition: 1,
-      thirdPosition: 2
+      firstPosition: 0
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -15,33 +13,11 @@ export default class ThumbnailGallery extends React.Component {
     const id = event.target.id;
     const arrLength = this.props.products.length;
     const firstPosition = this.state.firstPosition;
-    const secondPosition = this.state.secondPosition;
-    const thirdPosition = this.state.thirdPosition;
     if (id === 'chevRight') {
       if (firstPosition === arrLength - 1) {
-        this.setState({
-          firstPosition: 0,
-          secondPosition: 1,
-          thirdPosition: 2
-        });
-      } else if (firstPosition === arrLength - 2) {
-        this.setState({
-          firstPosition: firstPosition + 1,
-          secondPosition: 0,
-          thirdPosition: 1
-        });
-      } else if (firstPosition === arrLength - 3) {
-        this.setState({
-          firstPosition: firstPosition + 1,
-          secondPosition: secondPosition + 1,
-          thirdPosition: 0
-        });
+        this.setState({ firstPosition: 0 });
       } else {
-        this.setState({
-          firstPosition: firstPosition + 1,
-          secondPosition: secondPosition + 1,
-          thirdPosition: thirdPosition + 1
-        });
+        this.setState({ firstPosition: firstPosition + 1 });
       }
     } else if (id === 'chevLeft') {
       if (firstPosition === 0) {
@@ -54,10 +30,16 @@ export default class ThumbnailGallery extends React.Component {
 
   render() {
     const { titleLink, title, products } = this.props;
-    const product1 = products[0];
-    console.log('product 1', product1.productId);
     const display = '';
-    const { firstPosition, secondPosition, thirdPosition } = this.state;
+    const { firstPosition } = this.state;
+    let items = products.slice(firstPosition, firstPosition + 3);
+    if (products.length < 3) {
+      items = products;
+    } else if (items.length === 2) {
+      items = items.concat(products.slice(0, 1));
+    } else if (items.length === 1) {
+      items = items.concat(products.slice(0, 2));
+    }
     return (
       <>
         <div className='carousel-title text-center'>
@@ -68,27 +50,18 @@ export default class ThumbnailGallery extends React.Component {
         <div className='d-flex flex-row align-items-center justify-content-between'>
           <i onClick={this.handleClick} className="fas fa-chevron-left pr-2" id='chevLeft'></i>
           <div className='row d-flex justify-content-around'>
-            <div className={display}>
-              <div className='col-4 justify-content-center p-0'>
-                <a href={`#details?productId=${products.productId}`}>
-                  <img src={products.pictureUrl} className='home-image mx-2'></img>
-                </a>
-              </div>
-            </div>
-            <div className={display}>
-              <div className='col-4 justify-content-center p-0'>
-                <a href={`#details?productId=${products.productId}`}>
-                  <img src={products.pictureUrl} className='home-image mx-2'></img>
-                </a>
-              </div>
-            </div>
-            <div className={display}>
-              <div className='col-4 justify-content-center p-0'>
-                <a href={`#details?productId=${products.productId}`}>
-                  <img src={products.pictureUrl} className='home-image mx-2'></img>
-                </a>
-              </div>
-            </div>
+            {items.map((product, index) => {
+              return (
+                <div className={display} key={product.productId}>
+                  <div className='col-4 justify-content-center p-0'>
+                    <a href={`#details?productId=${product.productId}`}>
+                      <img src={product.pictureUrl} className='home-image mx-2'></img>
+                    </a>
+                  </div>
+                </div>
+              );
+            })
+            }
           </div>
           <i onClick={this.handleClick} className='fas fa-chevron-right pl-2' id='chevRight'></i>
         </div>
@@ -96,3 +69,10 @@ export default class ThumbnailGallery extends React.Component {
     );
   }
 }
+
+// const items = props.products.slice(firstItem, firstItem + 3);
+// if (items.length === 2) {
+//   items.concat(props.products.slice(0, 1));
+// } else if (items.length === 1) {
+//   items.concat(props.products);
+// }
